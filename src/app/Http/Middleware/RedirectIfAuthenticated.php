645f5be6ea0e1677e14e6d19108867d8eab7562f
TomoeHayano <ynyww0602@yahoo.co.jp>
2025-11-21 22:59:05 +0900

    20251121CSV

--- src/app/Http/Middleware/RedirectIfAuthenticated.php
+++ src/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,6 +6,7 @@ use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 class RedirectIfAuthenticated
 {
@@ -23,6 +24,12 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
+                $user = Auth::guard($guard)->user();
+
+                if ($user instanceof MustVerifyEmail && ! $user->hasVerifiedEmail()) {
+                    return redirect()->route('verification.notice');
+                }
+
                 return redirect(RouteServiceProvider::HOME);
             }
         }
